c53349d8d44ecbfd80becb69e8677a8c28bf95c8
Kishan <53341638+bangsikishan@users.noreply.github.com>
2022-06-21 21:48:22 +0545

    Deleted scripts folder

--- scripts/getData.js
+++
@@ -1,6 +0,0 @@
-// Get movie data based on search query
-const getData = async (query) => {
-    let response = await fetch(`http://www.omdbapi.com/?apikey=[YOUR-API-KEY]&s=${query}`);
-    let data = await response.json();
-    return data.Search[0];
-}
